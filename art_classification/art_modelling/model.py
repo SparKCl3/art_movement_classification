a5c699c2bb5e7b176478491d4036b30751eb4c11
Felipe Gandara <felipe.gandara1@gmail.com>
2024-11-27 11:20:18 +0100

    Model py packaging finished

--- art_classification/art_modelling/model.py
+++ art_classification/art_modelling/model.py
@@ -71,38 +71,39 @@ def compile_model(model: Model, learning_rate=0.0005) -> Model:
 
 def train_model(
         model: Model,
-        X: np.ndarray,
-        y: np.ndarray,
+        train_ds,
         batch_size=256,
-        patience=2,
+        patience=5,
         validation_data=None, # overrides validation_split
         validation_split=0.3
     ) -> Tuple[Model, dict]:
+
     """
     Fit the model and return a tuple (fitted_model, history)
     """
+
     # $CODE_BEGIN
+
     print(Fore.BLUE + "\nTraining model..." + Style.RESET_ALL)
 
     es = EarlyStopping(
-        monitor="val_loss",
         patience=patience,
         restore_best_weights=True,
         verbose=1
     )
 
     history = model.fit(
-        X,
-        y,
-        validation_data=validation_data,
+        train_ds,
+        batch_size = batch_size,
+        epochs = 1000,
+        validation_data = validation_data,
         validation_split=validation_split,
-        epochs=100,
-        batch_size=batch_size,
         callbacks=[es],
-        verbose=0
+        verbose=1
     )
+
     # $CODE_END
 
-    print(f"✅ Model trained on {len(X)} rows with min val MAE: {round(np.min(history.history['val_mae']), 2)}")
+    print(f"✅ Model trained with min val accuracy: {round(np.min(history.history['val_accuracy']), 2)}")
 
     return model, history
