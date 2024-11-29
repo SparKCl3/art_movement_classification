4aefbfedc9476b555ba286698a467a15ad3938fe
SparKCl3 <clothildesmorin@gmail.com>
2024-11-29 17:17:24 +0100

    updates

--- art_classification/api/api.py
+++ art_classification/api/api.py
@@ -5,3 +5,13 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Connected API"}
+
+
+#functions pour le main
+#processing image api
+#model
+#finetuner model
+#docker
+#api
+#streamlit
+#slides
